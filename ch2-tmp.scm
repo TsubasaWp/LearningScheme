6087084b1fef97bec32f0bc39976898e40647ef2
U-TENCENT\tsubasawang <tsubasawang@tsubasawang-PC2.tencent.com>
2016-05-01 21:07:12 +0800

    update

--- ch2-tmp.scm
+++ ch2-tmp.scm
@@ -36,11 +36,11 @@
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
 (define (map p sequence)
-  (accumulate (lambda (x y) (cons (p x) y)) () sequence))
+  (accumulate (lambda (x y) (cons (p x) y)) (list) sequence))
 (define (length sequence)
   (accumulate (lambda (x y) (+ y 1)) 0 sequence))
 (define (filter predicate sequence)
-  (cond ((null? sequence) ())
+  (cond ((null? sequence) (list))
         ((predicate (car sequence))
          (cons (car sequence)
                (filter predicate (cdr sequence))))
@@ -83,4 +83,112 @@
   (filter (lambda (x) (not (= x item)))
           sequence))
 (permutations (list 1 2 3))
+
+;; 2.40
+(enumerate-interval 1 10)
+
+(define (unique-pairs low high)
+  (flatmap (lambda (x) 
+         (map (lambda (y) (list x y))
+              (enumerate-interval (+ x 1) high)))
+           (enumerate-interval low high)))
+(unique-pairs 1 3);;=> ((1 2) (1 3) (2 3))
+(define (prime-sum-pair n)
+  (filter prime-sum? (unique-pairs 1 n)))
+(prime-sum-pair 3)
+
+;; 2.41
+(define (unique-triples low high)
+  (flatmap (lambda (x)
+             (map (lambda (i) (append x i)) (unique-pairs (+ x 1) high)))
+           (enumerate-interval low high)))
+(unique-triples 1 4)
+(define (prime-sum-triple? list)
+  (prime? (+ (car list) (cadr list) (cadr (cdr list)))))
+(define (prime-sum-triples n)
+  (filter prime-sum-triple? (unique-triples 1 n)))
+(prime-sum-triples 4) ;;=> ((1 2 4) (2 3 4))
+
+;; 2.42
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= 0 k)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+;; 每种摆法是一个排列
+(define (unique? sequence k)
+  (if (null? sequence)
+      #t
+      (if (= k (car sequence))
+          #f
+          (unique? (cdr sequence) k))))
+;; empty
+(define empty-board (list))
+;; adjoin
+(define (adjoin-position new-row k rest-of-queens)
+  (if (unique? rest-of-queens new-row)
+      (cons new-row rest-of-queens)))
+;; safe
+(define (safe? k positions) #t)
+;; 
+(queens 3)
+
+;; round-list
+(define (equal-pair p1 p2)
+  (cond ((and (= (car p1) (car p2)) (= (cdr p1) (cdr p2))) #t)
+        ((and (= (car p1) (cdr p2)) (= (cdr p1) (car p2))) #t)
+        (else #f)))
+
+(define (empty-rounds team-size)
+  (if (= team-size 0)
+      (list)
+      (append (list (list)) (empty-rounds (- team-size 1)))))
+(empty-rounds 3)
+
+(define (round-list team-size)
+  (define (round-col k)
+    (if (= 0 k)
+        (empty-rounds team-size)
+        (flatmap
+         (lambda (rest)
+           (map (lambda (next)
+                  (adjoin next k rest))
+                (enumerate-interval 1 team-size)))
+         (round-col (- k 1)))
+        ))
+  (round-col team-size))
+(round-list 4)
+
+(define (round-col k)
+  (if (= 0 k)
+      (empty-rounds 4)
+      (map
+       (lambda (rest)
+         (map (lambda (next)
+                (append rest (list next))
+                )
+              (enumerate-interval 1 4)))
+       (round-col (- k 1)))
+      ))
+(round-col 2)
+
+
+
+(define (adjoin next k rest)
+;;  (if (not (= k next))
+      (list (cons k next) rest))
+;;  (list rest))
+  (map (lambda (next)
+         (adjoin next 3 (list (cons 1 2))))
+       (enumerate-interval 1 3))
+
+
 
